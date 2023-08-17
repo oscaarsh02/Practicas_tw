309b21f74ca5a0d1acf16c5006ab42f85c374350
oscarsanchezhernandez <oscaar.sh02@gmail.com>
2023-08-17 17:31:10 -0600

    Proyecto terminado

--- p00/src/main.js
+++ p00/src/main.js
@@ -1,11 +1,14 @@
-<script>
-        
-var nombre = prompt ('nombre', ' ');
-var edad = prompt ('edad', 0);
+function getDatos()
+{
+    var nombre = prompt('Nombre', '');
+    var edad = prompt('Edad', 0);
 
-var div1 = document.getElementById('nombre');
-div1.innerHTML = '<h3> nombre: '+nombre+'</h3>'
+    var div1 = document.getElementById('nombre');
+    div1.innerHTML = '<h3> Nombre: '+nombre+' </h3>';
 
-var div2 = document.getElementById('edad');
-div2.innerHTML = '<h3> nombre: '+edad+'</h3>'
-</script>
+    var div2 = document.getElementById('edad');
+    div2.innerHTML = '<h3> Edad: '+edad+' </h3>';
+
+
+
+}
